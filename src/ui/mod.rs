6157f8e364f345cd1bf749477999018b9a5122c6
faervan <blackhunter77@gmx.de>
2024-09-19 21:50:55 +0200

    feat: add socket creation to server

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,8 +3,8 @@ use bevy::prelude::*;
 use crate::AppState;
 
 pub const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+pub const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+pub const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
 
 pub struct UiPlugin;
 
