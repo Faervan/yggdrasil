bf2c063993098353095c37c7ce83ea7af65e3fd2
faervan <blackhunter77@gmx.de>
2024-09-27 17:34:32 +0200

    dev sync

--- ysync/yclient/src/main.rs
+++ ysync/yclient/src/main.rs
@@ -2,6 +2,6 @@ use ysync::client::ConnectionSocket;
 
 fn main() {
     println!("Hello, world!");
-    let x = ConnectionSocket::build("127.0.0.1:9983", "stk0".to_string());
+    let x = ConnectionSocket::build("91.108.102.51:9983", "stk0".to_string());
     println!("{x:#?}");
 }
--- ysync/yserver/src/main.rs
+++ ysync/yserver/src/main.rs
@@ -2,6 +2,6 @@ use ysync::server::listen;
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
-    listen("127.0.0.1:9983").await?;
+    listen("0.0.0.0:9983").await?;
     Ok(())
 }
